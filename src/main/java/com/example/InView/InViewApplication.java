7cb91864d88722318ecf97a96c77ed230cbecb0d
Miemat <Miemat@github.com>
2022-01-06 16:17:48 +0100

    Init

---
+++ src/main/java/com/example/InView/InViewApplication.java
@@ -0,0 +1,13 @@
+package com.example.InView;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InViewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InViewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/InView/InViewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.InView;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InViewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
