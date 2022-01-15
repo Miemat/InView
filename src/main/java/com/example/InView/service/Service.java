98924de43f18c97a202697e015adb4541ed411d9
Miemat <mat.miernik@hotmail.com>
2022-01-16 00:19:07 +0100

    Sort Value

--- src/main/java/com/example/InView/service/Service.java
+++ src/main/java/com/example/InView/service/Service.java
@@ -2,8 +2,13 @@ package com.example.InView.service;
 
 import com.example.InView.utils.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.websocket.server.PathParam;
 
 @RestController
 public class Service {
@@ -18,9 +23,9 @@ public class Service {
     }
 
     @RequestMapping("/sortText")
-    public String sortText(){
+    public String sortText(@PathVariable String value){
 
-        return "test2";
+        return util.sortAscValue(value);
     }
 
 }
--- src/main/java/com/example/InView/utils/Utils.java
+++ src/main/java/com/example/InView/utils/Utils.java
@@ -2,6 +2,9 @@ package com.example.InView.utils;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 @Service
 public class Utils {
 
@@ -16,6 +19,30 @@ public class Utils {
         }
 
         return parametr;
+    }
+
+    public String sortAscValue(String value){
+
+        return sortString(value);
+    }
+
+    public String sortDescValue(){
+
+        if("DESC".equals(parametr)){
+            parametr="ASC";
+        }else if("ASC".equals(parametr)){
+            parametr="DESC";
+        }
+
+        return parametr;
+    }
+
+    public static String sortString(String inputString)
+    {
+        char tempArray[] = inputString.toCharArray();
+        Arrays.sort(tempArray);
+
+        return new String(tempArray);
     }
 
 }
